9e6cc5dd37ddb9df647779f9117d7e0be2f15c35
pawelpt <trepka.pawel@gmail.com>
2026-02-24 18:31:42 +0100

    update core/tray.js

--- app/core/tray.js
+++ app/core/tray.js
@@ -1,20 +1,21 @@
-import { Tray, Menu } from 'electron';
+import { Tray, Menu, app } from 'electron';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-// ESM-compatible __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export function setupTray(win) {
-  const tray = new Tray(path.join(__dirname, '../assets/icon/icon.png'));
+  const iconPath = path.join(app.getAppPath(), 'app/assets/icon/icon.png');
 
-  const menu = Menu.buildFromTemplate([
-    { label: _('Show'), click: () => win.show() },
-    { label: _('Hide'), click: () => win.hide() },
-    { label: _('Quit'), click: () => win.close() }
+  const tray = new Tray(iconPath);
+
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Show', click: () => win.show() },
+    { label: 'Hide', click: () => win.hide() },
+    { type: 'separator' },
+    { label: 'Quit', click: () => app.quit() }
   ]);
 
-  tray.setContextMenu(menu);
+  tray.setToolTip('WhatsWebApp Wrapper');
+  tray.setContextMenu(contextMenu);
+
+  tray.on('click', () => win.show());
 }
 
